b2e06da4ffc927c10d2f8612a0872006b963e11d
任振铭 <renzhenming@192.168.0.103>
2021-01-15 08:35:16 +0800

    path生成

--- arouter-compiler/src/main/java/com/rzm/arouter_compiler/ARouterProcessor.java
+++ arouter-compiler/src/main/java/com/rzm/arouter_compiler/ARouterProcessor.java
@@ -3,6 +3,12 @@ package com.rzm.arouter_compiler;
 import com.google.auto.service.AutoService;
 import com.rzm.arouter_annotations.ARouter;
 import com.rzm.arouter_annotations.bean.RouterBean;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterizedTypeName;
+import com.squareup.javapoet.TypeName;
+import com.squareup.javapoet.TypeSpec;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,6 +26,7 @@ import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.Name;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.TypeMirror;
@@ -36,6 +43,7 @@ public class ARouterProcessor extends AbstractProcessor {
     private Map<String, String> options;
     private Types typeUtils;
     private String moduleName;
+    private String aptPackage;
 
     // 仓库一 Path  缓存一
     // Map<"personal", List<RouterBean>>
@@ -54,6 +62,10 @@ public class ARouterProcessor extends AbstractProcessor {
         options = processingEnvironment.getOptions();
         typeUtils = processingEnvironment.getTypeUtils();
         messager.printMessage(Diagnostic.Kind.NOTE, "ARouterProcessor init");
+
+        // 只有接受到 App壳 传递过来的数据，才能证明我们的 APT环境搭建完成
+        moduleName = options.get(ProcessorConfig.MODULE_NAME);
+        aptPackage = options.get(ProcessorConfig.APT_PACKAGE);
     }
 
     @Override
@@ -75,7 +87,6 @@ public class ARouterProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
-        moduleName = options.get(ProcessorConfig.MODULE_NAME);
         messager.printMessage(Diagnostic.Kind.NOTE, "ARouterProcessor " + moduleName + " process");
         if (set.isEmpty()) {
             messager.printMessage(Diagnostic.Kind.NOTE, "ARouterProcessor process set is empty");
@@ -123,21 +134,98 @@ public class ARouterProcessor extends AbstractProcessor {
             }
 
             //传完整类名进去，得到type
-            TypeElement pathType = elementUtils.getTypeElement(ProcessorConfig.AROUTER_API_PATH); // ARouterPath描述
-            TypeElement groupType = elementUtils.getTypeElement(ProcessorConfig.AROUTER_API_GROUP); // ARouterGroup描述
+            TypeElement pathType = elementUtils.getTypeElement("com.rzm.arouter_api.ARouterPath"); // ARouterPath描述
+            TypeElement groupType = elementUtils.getTypeElement("com.rzm.arouter_api.ARouterGroup"); // ARouterGroup描述
 
-            createPathFile(pathType); // 生成 Path类
+            messager.printMessage(Diagnostic.Kind.NOTE, "pathType = " + pathType);
+            messager.printMessage(Diagnostic.Kind.NOTE, "groupType = " + groupType);
+
+            try {
+                createPathFile(pathType); // 生成 Path类
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
             createGroupFile(groupType, pathType);
         }
         return true;
     }
 
-    private void createGroupFile(TypeElement groupType, TypeElement pathType) {
+    /**
+     * public class ARouter$$Path$$app implements ARouterPath {
+     * *   @Override
+     * *   public Map<String, RouterBean> getPathMap() {
+     * *     Map<String, RouterBean> pathMap = new HashMap<>();
+     * *     pathMap.put("/app/MainActivity", RouterBean.create(RouterBean.TypeEnum.ACTIVITY, MainActivity.class, "/app/MainActivity", "app"));
+     * *     pathMap.put("/app/Main2Activity", RouterBean.create(RouterBean.TypeEnum.ACTIVITY, Main2Activity.class, "/app/Main2Activity", "app"));
+     * *     return pathMap;
+     * *   }
+     * * }
+     *
+     * @param pathType
+     */
+    private void createPathFile(TypeElement pathType) throws IOException {
+        if (mAllPathMap == null || mAllPathMap.size() == 0) {
+            return;
+        }
 
+        ParameterizedTypeName getPathMapMethodReturn = ParameterizedTypeName.get(
+                ClassName.get(Map.class),
+                ClassName.get(String.class),
+                ClassName.get(RouterBean.class)
+        );
+
+        Set<Map.Entry<String, List<RouterBean>>> entries = mAllPathMap.entrySet();
+        for (Map.Entry<String, List<RouterBean>> entry : entries) {
+            MethodSpec.Builder getPathMapBuilder = MethodSpec.methodBuilder(ProcessorConfig.PATH_METHOD_NAME)
+                    .addModifiers(Modifier.PUBLIC)
+                    .addAnnotation(Override.class)
+                    .returns(getPathMapMethodReturn);
+
+            //T 代表 class， N 代表引用 S代表字符串 L代表枚举
+            getPathMapBuilder.addStatement(
+                    "$T<$T,$T> $N = new $T()",
+                    ClassName.get(Map.class),
+                    ClassName.get(String.class),
+                    ClassName.get(RouterBean.class),
+                    ProcessorConfig.PATH_VAR1,
+                    ClassName.get(HashMap.class)
+            );
+
+            List<RouterBean> routerBeans = entry.getValue();
+            for (RouterBean routerBean : routerBeans) {
+                getPathMapBuilder.addStatement(
+                        "$N.put($S, $T.create($T.$L, $T.class, $S, $S))",
+                        ProcessorConfig.PATH_VAR1,
+                        routerBean.getPath(),
+                        ClassName.get(RouterBean.class),
+                        ClassName.get(RouterBean.TypeEnum.class),
+                        routerBean.getTypeEnum(),
+                        ClassName.get((TypeElement) routerBean.getElement()),
+                        routerBean.getPath(),
+                        routerBean.getGroup()
+                );
+            }
+
+            getPathMapBuilder.addStatement("return $N", ProcessorConfig.PATH_VAR1);
+
+            // 最终生成的类文件名  ARouter$$Path$$personal
+            String finalClassName = ProcessorConfig.PATH_FILE_NAME + entry.getKey();
+            messager.printMessage(Diagnostic.Kind.NOTE, "APT生成路由Path类文件：" +
+                    aptPackage + "." + finalClassName);
+            TypeSpec classType = TypeSpec.classBuilder(finalClassName)
+                    .addMethod(getPathMapBuilder.build())
+                    .addSuperinterface(ClassName.get(pathType))
+                    .addModifiers(Modifier.PUBLIC)
+                    .build();
+
+            JavaFile.builder(aptPackage, classType).build().writeTo(filer);
+
+            mAllGroupMap.put(entry.getKey(), finalClassName);
+        }
     }
 
-    private void createPathFile(TypeElement pathType) {
+    private void createGroupFile(TypeElement groupType, TypeElement pathType) {
 
     }
 
